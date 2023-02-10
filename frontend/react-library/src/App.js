99857ba05390288937b40cb19c95df9e3b53e8a4
Sagar <sagar.jobs@outlook.com>
2023-02-10 11:31:47 +1100

    Update styling in App.js

--- frontend/react-library/src/App.js
+++ frontend/react-library/src/App.js
@@ -7,15 +7,17 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <div className='d-flex flex-column min-vh-100'>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Navigate to='/home' />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/searchbooks" element={<SearchBooksPage />} />
-      </Routes>
+      <div className='flex-grow-1'>
+        <Routes>
+          <Route path='/' element={<Navigate to='/home' />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/searchbooks" element={<SearchBooksPage />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
